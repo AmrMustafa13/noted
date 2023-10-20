0cf5e3cfa4e979dc9d29f72a60e562842305d023
Amr Mustafa <amrmostafa1311@gmail.com>
2023-10-20 20:42:59 +0300

    added morgan logger

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 dotenv.config({
   path: "./config/config.env",
@@ -8,6 +9,7 @@ dotenv.config({
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
